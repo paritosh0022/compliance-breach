208cbf31af7e582cbaf36c37a86cabc8b0ea1b5d
paritosh mishra <paritoshmishra4002@gmail.com>
2025-07-14 07:22:47 +0000

    replace device hub on side bar with the svg code given below do not make

--- src/components/dashboard-sidebar.tsx
+++ src/components/dashboard-sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import React from 'react';
@@ -16,7 +17,30 @@ export default function DashboardSidebar() {
   return (
     <Sidebar>
       <SidebarHeader className="p-4">
-        <h2 className="text-2xl font-headline font-semibold">Device Hub</h2>
+        <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1184.37 200.65" className="h-10 w-auto">
+          <defs>
+            <style>
+              {`
+                .cls-1-logo { fill: #b38a2f; stroke-width: 0px; }
+                .cls-2-logo { fill: #c4223d; stroke-width: 0px; }
+              `}
+            </style>
+          </defs>
+          <g>
+            <path className="cls-2-logo" d="m444.11,195.64L297.45,31.45v163.64h-14.75V0l10.57,5.28,145.83,163.64V5.28h14.75v195.37l-9.74-5.01Z"/>
+            <path className="cls-2-logo" d="m497.27,195.09V7.24l14.75-5.57v193.42h-14.75Z"/>
+            <path className="cls-2-logo" d="m617.77,199.54l-5.85-4.73L534.27,6.95l13.36-5.57,68.75,164.48L684.83,1.39l13.08,5.57-80.14,192.58Z"/>
+            <path className="cls-2-logo" d="m721.02,195.09V5.28h103.53l-5.3,12.25h-83.49v75.97h80.16v12.25h-80.16v77.09h86.83l4.18,12.25h-105.75Z"/>
+            <path className="cls-2-logo" d="m989.58,17.53h-69.02v177.55h-14.74V17.53h-73.48l4.45-12.25h157.24l-4.45,12.25Z"/>
+            <path className="cls-2-logo" d="m1145.98,17.53h-69.03v177.55h-14.74V17.53h-73.48l4.45-12.25h157.25l-4.45,12.25Z"/>
+            <path className="cls-2-logo" d="m1169.63,195.09V7.24l14.74-5.57v193.42h-14.74Z"/>
+          </g>
+          <g>
+            <polygon className="cls-1-logo" points="217.74 194.55 0 73.76 0 2.11 217.74 123.77 217.74 194.55"/>
+            <polygon className="cls-2-logo" points="0 193.49 0 119.32 74.18 159.16 0 193.49"/>
+            <polygon className="cls-2-logo" points="217.74 76.29 217.74 2.11 143.56 41.95 217.74 76.29"/>
+          </g>
+        </svg>
       </SidebarHeader>
       <SidebarContent className="p-2">
         <SidebarMenu>
